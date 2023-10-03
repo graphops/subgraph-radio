15e07641c7ca067ee5d6ca01cc28af64c0a4218b
axiomatic-aardvark <pavlovskipetko@abv.bg>
2023-10-03 15:05:19 +0300

    feat: serve frontend

--- subgraph-radio/src/main.rs
+++ subgraph-radio/src/main.rs
@@ -4,12 +4,38 @@ use dotenv::dotenv;
 
 use graphcast_sdk::{graphcast_agent::GraphcastAgent, WakuMessage};
 use subgraph_radio::{config::Config, operator::RadioOperator, RADIO_OPERATOR};
+use tracing::info;
+use warp::serve;
+use warp::Filter;
 
 extern crate partial_application;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    // TODO: start frontend
+    let wasm_bytes = include_bytes!("qos-dashboard.wasm");
+    let html_bytes = include_bytes!("qos-dashboard.html");
+
+    let wasm = warp::path("qos-dashboard.wasm").and_then(move || async move {
+        let reply = warp::reply::with_header(&wasm_bytes[..], "content-type", "application/wasm");
+        Ok::<_, warp::Rejection>(reply)
+    });
+
+    let html = warp::path("index.html").and_then(move || async move {
+        let reply = warp::reply::with_header(&html_bytes[..], "content-type", "text/html");
+        Ok::<_, warp::Rejection>(reply)
+    });
+
+    let routes = wasm.or(html);
+
+    serve(routes)
+        .run(([127, 0, 0, 1], 3030)) // Change 3030 to your desired port
+        .await;
+
+    info!("got here");
+
     // Parse basic configurations
     let radio_config = Config::args();
 
